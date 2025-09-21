00dcbc7ce26a96fd0261fe44d799bdbaa8076b57
Avinash <avinashdola57@gmail.com>
2025-09-21 10:14:06 +0530

    Initial Commit with mail sender added

---
+++ sdp-backend/src/main/java/com/klef/sdp/SdpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.klef.sdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdpBackendApplication.class, args);
+    }
+
+}
